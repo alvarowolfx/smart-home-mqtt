8a4a328e60a0d33e93432c8bd6e5d26571e3bda7
alvarowolfx <alvarowolfx@gmail.com>
2020-02-15 18:32:44 -0400

    chore: Add eslint

--- webhook-worker/index.js
+++ webhook-worker/index.js
@@ -1,38 +1,39 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongodb = require('mongodb')
+
 const { MongoClient } = mongodb
 const mongoUrl = process.env.DB_URL
 
-async function run(){
-  const client = await MongoClient.connect(mongoUrl, { useNewUrlParser : true, useUnifiedTopology : true})
+async function run() {
+  const client = await MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
   const db = client.db('devices')
   const dataCollection = db.collection('device_data')
   const historyCollection = db.collection('device_history')
 
   const app = express()
-  const port = parseInt(process.env.PORT,10)
+  const port = parseInt(process.env.PORT, 10)
 
   app.use(bodyParser.json())
 
-  app.get('/_health', (req,res) => {
-    res.json({ result : 'ok'})
+  app.get('/_health', (req, res) => {
+    res.json({ result: 'ok' })
   })
 
   app.post('/webhook', async (req, res) => {
     try {
       const { client_id: deviceId, payload } = req.body
       const decoded = Buffer.from(payload, 'base64').toString('utf8')
-      const data =  JSON.parse(decoded)
-      await dataCollection.updateOne({ '_id' : deviceId }, { '$set' : data }, { upsert : true})
+      const data = JSON.parse(decoded)
+      await dataCollection.updateOne({ _id: deviceId }, { $set: data }, { upsert: true })
       await historyCollection.insertOne({
         ...data,
         deviceId,
-        time : new Date()
+        time: new Date(),
       })
-      res.json({ result : 'ok '})
-    }catch(err){
-      res.status(500).json({ message : err.message })
+      res.json({ result: 'ok' })
+    } catch (err) {
+      res.status(500).json({ message: err.message })
     }
   })
 
